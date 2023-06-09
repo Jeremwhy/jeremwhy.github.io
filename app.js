5fa7c112ac2b6c0a6f87ffb52c85de3741004769
Jeremwhy <134602473+Jeremwhy@users.noreply.github.com>
2023-06-09 13:18:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function (g, e, n, es, ys) {
+  (function (g, e, n, es, ys) {
     g['_genesysJs'] = e;
     g[e] = g[e] || function () {
       (g[e].q = g[e].q || []).push(arguments)
@@ -7,6 +7,6 @@
     g[e].c = es;
     ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
   })(window, 'Genesys', 'https://apps.mypurecloud.de/genesys-bootstrap/genesys.min.js', {
-    environment: 'euc1',
-    deploymentId: 'f269c139-6462-43ce-a3bb-1bdbe6064c8e'
+    environment: 'prod-euc1',
+    deploymentId: '4aeca6a4-3002-4968-8902-8d46f7a10095'
   });
